61a56be0215a171c5eb87a6ce5394588afa5be6a
admin <ddongdor@gmail.com>
2020-02-20 10:27:52 +0900

    Changed the post_list_html for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0@^00t0t*$ksd+1zjcbx2y+&sz=o#&f4%6v$siy$h(#jdiox^l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.pythonanywhere.com', '192.168.0.21']
 
 
 # Application definition
